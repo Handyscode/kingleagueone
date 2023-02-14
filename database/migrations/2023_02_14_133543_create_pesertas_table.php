2c6f249f52db86fc8e7a58a8718c6af119d19ff9
rabani2022 <kopsyahrabani@gmail.com>
2023-02-14 20:43:22 +0700

    add peserta model and migration

---
+++ database/migrations/2023_02_14_133543_create_pesertas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesertas', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo', 255);
+            $table->string('nama', 50);
+            $table->string('asal_tim', 50);
+            $table->string('kategori_usia', 10);
+            $table->string('no_punggung', 50);
+            $table->string('posisi', 50);
+            $table->string('foto_kk', 255);
+            $table->string('foto_akte', 255);
+            $table->string('foto_ijazah', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesertas');
+    }
+};
